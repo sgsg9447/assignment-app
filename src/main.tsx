d9bbffb971de24e047fc20882c01e78d2ceb306e
김슬기(seuling) <87474789+sgsg9447@users.noreply.github.com>
2024-06-04 14:11:09 +0900

    Feature/routes (#2)
    * chore: add react-router-dom and qs package
    
    * feat: add useRouter hook for navigation
    
    * feat: initialize routing setup and add page components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import  Layout  from "./Layout.tsx";
+import Layout from "./Layout.tsx";
 import "./styles/reset.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes } from "./pages/Routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Layout />
+    <Router>
+      <Layout>
+        <Routes />
+      </Layout>
+    </Router>
   </React.StrictMode>
 );
